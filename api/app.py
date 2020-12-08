e46a6047f2e66a9fe6efa0cf13a3017a7a9ffd50
dimatrubca <dimatrubca@gmail.com>
2020-12-09 00:33:42 +0200

    add config file

--- api/app.py
+++ api/app.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from api.controller.router import router as api_router
 
+from config import PROJECT_NAME, LOG_LEVEL
 import uvicorn
 import logging
 
 
-def get_application() -> FastAPI:
-    app = FastAPI(title='AI-Chatbot-API', debug=True)
+def get_app() -> FastAPI:
+    app = FastAPI(title=PROJECT_NAME, debug=True)
 
     app.add_middleware(
         CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
@@ -17,12 +18,12 @@ def get_application() -> FastAPI:
 
     return app
 
-app = get_application()
+app = get_app()
 
 # logging
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(LOG_LEVEL)
 logger.info("Open http://127.0.0.1:8000/docs to see Swagger API Documentation.")
 
 if __name__ == "__main__":
